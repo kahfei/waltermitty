ea3474f4c3c35b4712dda6b50403459320ac9fc0
kahfei <bak.kahfei@gmail.com>
2014-07-15 18:26:44 +0800

    cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,16 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.count.zero?
+      return true
+    else
+      errors[:base] << "Line Items present"
+      return false
+    end
+  end
+
+
 end
